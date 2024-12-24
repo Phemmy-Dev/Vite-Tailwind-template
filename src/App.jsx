cf5b52273b5acb3e8aade4792261f8b30822c572
Phemmy <phemmy123x@gmail.com>
2024-12-24 09:51:30 +0100

    Text Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-900">
-      Hello world!
+      <h1 className="text-4xl font-bold h-screen w-screen flex justify-center items-center text-red-900">
+      Happy Coding! 🚀
     </h1>
     </>
   )
